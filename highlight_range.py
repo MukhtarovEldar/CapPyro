17069bd3f3a50c4f0a51239958eb3c1d612501fd
Eldar Mukhtarov <95146882+MukhtarovEldar@users.noreply.github.com>
2023-09-08 21:50:52 +0200

    Enhanced code performance and readability

--- highlight_range.py
+++ highlight_range.py
@@ -1,29 +1,29 @@
-def highlight_range(text, pos_start, pos_end):
+def highlight_range(text, pos_beg, pos_end):
     result = ''
 
     # Calculate the range of lines to process
-    idx_start = max(text.rfind('\n', 0, pos_start.idx), 0)
-    idx_end = text.find('\n', idx_start + 1)
-    if idx_end < 0:
-        idx_end = len(text)
+    index_beg = max(text.rfind('\n', 0, pos_beg.index), 0)
+    index_end = text.find('\n', index_beg + 1)
+    if index_end < 0:
+        index_end = len(text)
 
     # Generate each line's arrow
-    line_count = pos_end.ln - pos_start.ln + 1
-    for _ in range(line_count):
+    line_cnt = pos_end.line - pos_beg.line + 1
+    for _ in range(line_cnt):
         # Extract line and its range of columns
-        line = text[idx_start:idx_end]
-        col_start = pos_start.col if _ == 0 else 0
-        col_end = pos_end.col if _ == line_count - 1 else len(line) - 1
+        line = text[index_beg:index_end]
+        col_beg = pos_beg.col if _ == 0 else 0
+        col_end = pos_end.col if _ == line_cnt - 1 else len(line) - 1
 
         # Build the arrow line by line
         result += line + '\n'
-        result += ' ' * col_start + '^' * (col_end - col_start)
+        result += ' ' * col_beg + '^' * (col_end - col_beg)
 
         # Update line indices
-        idx_start = idx_end
-        idx_end = text.find('\n', idx_start + 1)
-        if idx_end < 0:
-            idx_end = len(text)
+        index_beg = index_end
+        index_end = text.find('\n', index_beg + 1)
+        if index_end < 0:
+            index_end = len(text)
 
     # Remove tabs and return the result
     return result.replace('\t', '')
